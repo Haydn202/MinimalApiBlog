50661058c4638befa740de186730065c6e85f90c
haydn202 <haydnthomson@hotmail.com>
2023-09-03 14:35:16 +1200

    database scructure

--- MinimalApiBlog/DbContexts/BlogDbContext.cs
+++ MinimalApiBlog/DbContexts/BlogDbContext.cs
@@ -18,34 +18,128 @@ namespace MinimalApiBlog.DbContexts
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure the Topic entity
+        modelBuilder.Entity<Topic>()
+            .HasKey(t => t.Id);
+
+        // Configure other entities and relationships here
+        modelBuilder.Entity<Article>()
+            .HasOne(a => a.Topic)
+            .WithMany()
+            .HasForeignKey(a => a.TopicId);
+
+        modelBuilder.Entity<Article>()
+            .HasOne(a => a.Author)
+            .WithMany()
+            .HasForeignKey(a => a.AuthorId);
+
+        modelBuilder.Entity<Article>()
+            .HasMany(a => a.Comments)
+            .WithOne()
+            .HasForeignKey(c => c.ArticleId);
+
+        modelBuilder.Entity<MainComment>()
+            .HasMany(mc => mc.SubComments)
+            .WithOne()
+            .HasForeignKey(sc => sc.MainCommentId);
+
+        // Seed data
+        SeedData(modelBuilder);
+    }
+
+    private void SeedData(ModelBuilder modelBuilder)
+    {
+        var topic1 = new Topic { Id = Guid.NewGuid(), Name = "Technology" };
+        var topic2 = new Topic { Id = Guid.NewGuid(), Name = "Science" };
+        var topic3 = new Topic { Id = Guid.NewGuid(), Name = "Travel" };
+        
+        // Sample Topics
+        modelBuilder.Entity<Topic>().HasData(
+            topic1,
+            topic2,
+            topic3
+        );
+
+        var author1 = new Author
+            { Id = Guid.NewGuid(), Name = "John Doe", Bio = "Tech Enthusiast", ProfileUrl = "https://example.com/johndoe" };
+        var author2 = new Author
+            { Id = Guid.NewGuid(), Name = "John Doe", Bio = "Tech Enthusiast", ProfileUrl = "https://example.com/johndoe" };
+        // Sample Authors
+        modelBuilder.Entity<Author>().HasData(
+            author1,
+            author2
+        );
+
+        var article1 = new Article
         {
-            base.OnModelCreating(modelBuilder);
+            Id = Guid.NewGuid(),
+            Title = "Introduction to AI",
+            Description = "A beginner's guide to Artificial Intelligence",
+            TopicId = topic1.Id,
+            AuthorId = author1.Id,
+            CreatedOn = DateTime.Now,
+            ContentUrl = "https://example.com/article1"
+        };
 
-            // Configure the Topic entity
-            modelBuilder.Entity<Topic>()
-                .HasKey(t => t.Id);
+        var article2 = new Article
+        {
+            Id = Guid.NewGuid(),
+            Title = "Space Exploration",
+            Description = "Discovering the wonders of the universe",
+            TopicId = topic2.Id,
+            AuthorId = author2.Id,
+            CreatedOn = DateTime.Now,
+            ContentUrl = "https://example.com/article2"
+        };
 
-            // Configure other entities and relationships here
-            modelBuilder.Entity<Article>()
-                .HasOne(a => a.Topic)
-                .WithMany()
-                .HasForeignKey(a => a.TopicId);
+        // Sample Articles
+        modelBuilder.Entity<Article>().HasData(
+            article1,
+            article2
+        );
 
-            modelBuilder.Entity<Article>()
-                .HasOne(a => a.Author)
-                .WithMany()
-                .HasForeignKey(a => a.AuthorId);
+        // var comment1 = new MainComment
+        // {
+        //     Id = Guid.NewGuid(),
+        //     Message = "Great article!",
+        //     ArticleId = article1.Id,
+        //     CreatedOn = DateTime.Now
+        // };
+        // var comment2 = new MainComment
+        // {
+        //     Id = Guid.NewGuid(),
+        //     Message = "I found this very informative.",
+        //     ArticleId = article1.Id,
+        //     CreatedOn = DateTime.Now
+        // };
+        //
+        // // Sample MainComments
+        // modelBuilder.Entity<MainComment>().HasData(
+        //     comment1,
+        //     comment2
+        // );
 
-            modelBuilder.Entity<Article>()
-                .HasMany(a => a.Comments)
-                .WithOne()
-                .HasForeignKey(c => c.ArticleId);
-
-            modelBuilder.Entity<MainComment>()
-                .HasMany(mc => mc.SubComments)
-                .WithOne()
-                .HasForeignKey(sc => sc.MainCommentId);
+        // // Sample SubComments
+        // modelBuilder.Entity<SubComment>().HasData(
+        //     new SubComment
+        //     {
+        //         Id = Guid.NewGuid(),
+        //         Message = "Thank you!",
+        //         MainCommentId = comment2.Id,
+        //         CreatedOn = DateTime.Now
+        //     },
+        //     new SubComment
+        //     {
+        //         Id = Guid.NewGuid(),
+        //         Message = "I agree!",
+        //         MainCommentId = comment2.Id,
+        //         CreatedOn = DateTime.Now
+        //     }
+        // );
         }
     }
 }
--- MinimalApiBlog/Entities/Article.cs
+++ MinimalApiBlog/Entities/Article.cs
@@ -7,7 +7,8 @@ namespace MinimalApiBlog.Entities
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string ThumbnailUrl { get; set; }
+        public string ThumbnailUrl { get; set; } =
+            "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/640px-SpongeBob_SquarePants_character.svg.png";
         public DateTime CreatedOn { get; set; }
         public string ContentUrl { get; set; } 
         public Topic Topic { get; set; }
