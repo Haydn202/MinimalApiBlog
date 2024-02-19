a10abacfb8aca7eba506c25de08cfd1e0d10cf9a
haydn202 <haydnthomson@hotmail.com>
2024-02-19 17:30:10 +1300

    basic exception handler

--- MinimalApiBlog/Program.cs
+++ MinimalApiBlog/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.EntityFrameworkCore;
 using MinimalApiBlog.DbContexts;
 using MinimalApiBlog.Extensions;
@@ -11,7 +12,23 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler(configureApplicationBuilder =>
+    {
+        configureApplicationBuilder.Run(
+            async context =>
+            {
+                context.Response.StatusCode = (int)
+                    HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync("An unexpected problem happened.");
+            });
+    });
+}
+
 app.UseHttpsRedirection();
+
 app.RegisterArticlesEndpoints();
 app.RegisterTopicEndpoints();
 
